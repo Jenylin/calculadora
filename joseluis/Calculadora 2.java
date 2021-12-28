056dfb841151a5a03e124d7215fcdd1683bb48fe
JLuisSJ <96786841+JLuisSJ@users.noreply.github.com>
2021-12-28 13:12:14 -0600

    Update Calculadora 2.java
    .

--- joseluis/Calculadora 2.java
+++ joseluis/Calculadora 2.java
@@ -14,52 +14,52 @@ public class Calculadora {
 		System.out.println("******Has iniciado la calculadora******");
 		System.out.println("1.- Suma");
 		System.out.println("2.- Resta");
-		System.out.println("3.- Multiplicacion");
-		System.out.println("4.- Division");
-		System.out.println("Selecciona un numero de operacion: ");
+		System.out.println("3.- Multiplicación");
+		System.out.println("4.- División");
+		System.out.println("Selecciona un numero de operación: ");
 		int opcion;
 		opcion =  sc.nextInt();
 		
 		switch(opcion){
 			case 1:
-				System.out.print("Digita el primer numero: ");
+				System.out.print("Digita el primer número: ");
 				num_uno = sc.nextDouble();
-				System.out.print("Digita el segundo numero: ");
+				System.out.print("Digita el segundo número: ");
 				num_dos = sc.nextDouble();
 				resultado = num_uno + num_dos;
-				System.out.println("La suma de los numeros es: " + resultado);				
+				System.out.println("La suma de los números es: " + resultado);				
 				break;
 			case 2:
-				System.out.print("Digita el primer numero: ");
+				System.out.print("Digita el primer número: ");
 				num_uno = sc.nextDouble();
-				System.out.print("Digita el segundo numero: ");
+				System.out.print("Digita el segundo número: ");
 				num_dos = sc.nextDouble();
 				resultado = num_uno - num_dos;
-				System.out.println("La resta de los numeros es: " + resultado);
+				System.out.println("La resta de los números es: " + resultado);
 				break;
 			case 3:
-				System.out.print("Digita el primer numero: ");
+				System.out.print("Digita el primer número: ");
 				num_uno = sc.nextDouble();
-				System.out.print("Digita el segundo numero: ");
+				System.out.print("Digita el segundo número: ");
 				num_dos = sc.nextDouble();
 				resultado = num_uno * num_dos;
-				System.out.println("La multiplicacion de los numeros es: " + resultado);
+				System.out.println("La multiplicación de los números es: " + resultado);
 				break;
 			case 4:
-				System.out.print("Digita el primer numero: ");
+				System.out.print("Digita el primer número: ");
 				num_uno = sc.nextDouble();
-				System.out.print("Digita el segundo numero: ");
+				System.out.print("Digita el segundo número: ");
 				num_dos = sc.nextDouble();
 				resultado = num_uno / num_dos;
-				System.out.println("La division de los numeros es: " + resultado);
+				System.out.println("La división de los números es: " + resultado);
 				break;
 				default: 
-					System.out.println("Opcion invalida, vuelve a seleccionar una opcion correcta");
+					System.out.println("Opcion inválida, vuelve a seleccionar una opción correcta");
 					break;
 					
 		}	
-		System.out.print("\n�Quiere seguir?\n");
-		System.out.print("Presione �S� para continuar: ");
+		System.out.print("\n¿Quiere seguir?\n");
+		System.out.print("Presione ´S´ para continuar: ");
 		tecla = new Scanner(System.in).nextLine();
 		} while (tecla.equals("s") || tecla.equals("S"));
 		
