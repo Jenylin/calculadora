7df5106fa1c2a8a465543e17243ffe033de30176
fabi2398 <96786664+fabi2398@users.noreply.github.com>
2022-01-18 18:24:39 -0600

    Add files via upload

---
+++ fabian/calculadoras/src/main/java/calcu/sumas.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calcu;
+
+/**
+ *
+ * @author fabian
+ */
+public class sumas {
+    
+      public double result;
+   
+    
+    public void sumar  (double n1 ,double n2){
+        
+        
+        for(int i=0;n1 <= n2 ;i++){
+        result=i+n2;
+         
+         
+        }
+    }
+}
